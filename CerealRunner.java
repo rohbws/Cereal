7d5a0d70a77a99de0e1b298850dec85b9ce02f88
boswo <bosworthrohan@gmail.com>
2021-03-18 10:21:43 -0700

    Added runner

---
+++ CerealRunner.java
@@ -0,0 +1,9 @@
+public class CerealRunner {
+    public static void main(String[] args) {
+        Cereal cocoaPuffs = new Cereal("Cocoa Puffs", 12);
+        Cereal allBran = new Cereal("All Bran", 7);
+        Cereal Bran100 = new Cereal("100% Bran", 5);
+
+        System.out.println(allBran);
+    }
+}
