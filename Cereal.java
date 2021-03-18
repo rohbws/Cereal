c830ceab5145271096db9e51217701b38e135c52
Rohan_B <bosworthrohan@gmail.com>
2021-03-18 10:10:15 -0700

    Add files via upload

---
+++ Cereal.java
@@ -0,0 +1,18 @@
+public class Cereal
+{
+    private String name;
+    private double carbs;
+
+    public Cereal(String name, double carbs){
+        this.name = name;
+        this.carbs = carbs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+}
